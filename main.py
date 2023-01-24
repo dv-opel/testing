bf0aad9e8ee1e7b621376fb4e36ebb7843fcfcce
Dmytro <d.verich1990@gmail.com>
2023-01-24 17:01:51 +0200

    second commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello")
+from turtle import *
+from random import randint
 
-print("new text")
+colors = ["red", "green", "yellow", "blue", "pink", "orange", "aqua", "lightgreen"]
+
+t = Turtle()
+t.width(6)
+
+
+for i in range(36):
+    t.color(colors[randint(0, len(colors)-1)])
+    t.circle(80)
+    t.left(10)
+
+done()
